dc4de821ad57d6b0426bd33891b9238fdeb2fdfc
mofopeadegoke <mofopeadegoke@gmail.com>
2025-03-17 21:02:47 +0200

    mobile nav bar practically done

--- my-next-app/src/app/components/navbar/index.tsx
+++ my-next-app/src/app/components/navbar/index.tsx
@@ -1,18 +1,42 @@
 import Image from "next/image";
 import logo from "../../../../public/cultureCapsuleLogo.png";
 import search from "../../../../public/mgGlass.png";
+import burger from "../../../../public/burger.png";
+import Link from "next/link";
 import BtnColor from "../btnColor";
+import { useState, useEffect, useRef } from "react";
 import "./style.css";
 export default function Navbar() {
+  const setShow = useState(false)[1]; // This is the setter function in which the state value cannot be accessed directly except through the setter function
+  const mobContentEl = useRef<HTMLElement | null>(null);
+  const burgBtn = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const burgBtnEl = burgBtn.current;
+    if (!burgBtnEl || !mobContentEl.current) return;
+
+    const handleClick = () => {
+      setShow((prev) => {
+        const newState = !prev;
+        mobContentEl.current!.style.display = newState ? "flex" : "none";
+        return newState;
+      });
+    };
+    burgBtnEl.addEventListener("click", handleClick);
+
+    return () => {
+      burgBtnEl.removeEventListener("click", handleClick);
+    };
+  }, []);
   return (
-    <nav>
+    <nav className="navMain">
       <Image
         src={logo}
         alt="A feather with text which says culture capsule by the side"
         width={300}
         height={157.9}
       />
-      <ul>
+      <ul className="web">
         <li>
           <a href="#">Home</a>
         </li>
@@ -26,7 +50,7 @@ export default function Navbar() {
           <a href="#">Profile</a>
         </li>
       </ul>
-      <div className="row">
+      <div className="row web">
         <article className="srchCont">
           <input type="text" placeholder="Search" />
           <Image src={search} alt="A magnifying glass" width={24} height={24} />
@@ -34,6 +58,26 @@ export default function Navbar() {
         <div className="line"></div>
         <BtnColor content="Login" />
       </div>
+      <div className="burgBtn mobile" ref={burgBtn}>
+        <Image src={burger} alt="The burger icon" width={60} height={60} />
+      </div>
+      <section className="mobileNavMain" ref={mobContentEl}>
+        <Link href="/">Home</Link>
+        <div className="navRow">
+          <p>
+            Interests <span>&#xF22C;</span>
+          </p>
+        </div>
+        <section className="intCont">
+          <Link href="/food">Food & Gastronomy</Link>
+          <Link href="/arts">Arts & Culture</Link>
+          <Link href="/folklore">Folklore</Link>
+          <Link href="/events">Historical Events</Link>
+          <Link href="/crafts">Handicrafts</Link>
+        </section>
+        <Link href="/about">About</Link>
+        <Link href="/profile">Profile</Link>
+      </section>
     </nav>
   );
 }
